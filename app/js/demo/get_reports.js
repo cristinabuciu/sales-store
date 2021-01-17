c70291c0b73aacbf4a2b95d2dca1371c2e754e48
Roxana Balasoiu <balasoiu@adobe.com>
2021-01-17 17:34:27 +0200

    Added reports page

---
+++ app/js/demo/get_reports.js
@@ -0,0 +1,22 @@
+// $( document ).ready(function() {
+//     var cookie = $.cookie("username");
+//     if(cookie == null){
+//         window.location="login.html";
+//     }
+//     $("#userAddStock").text(cookie);
+//     console.log( cookie );
+// });
+
+function getOrdersReport() {
+
+}
+
+function getStockReport() {
+
+
+}
+
+function getUsersReport() {
+
+
+}
